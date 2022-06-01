69840ae412754227b46f54ce8911fcd7556f320d
Junior Egger <junior.egger@gmail.com>
2022-06-01 20:14:40 -0300

    class department

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -7,10 +7,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class DB {
 
+    private static final Logger log = Logger.getLogger(DB.class.getName());
+
     private static Connection conn = null;
+
+    private DB() {
+        //CONSTRUTOR VAZIO APONTADO PELO SONAR
+    }
 
     public static Connection getConnection() {
         if (conn == null) {
@@ -18,7 +25,7 @@ public class DB {
                 Properties props = loadProperties();
                 String url = props.getProperty("dburl");
                 conn = DriverManager.getConnection(url, props);
-                System.out.println("CONECTOU");
+                log.info("CONECTOU");
             } catch (SQLException e) {
                 throw new DBException(e.getMessage());
             }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 
+import model.entities.Department;
+
 import java.sql.Connection;
 
 import static db.DB.closeConnection;
 import static db.DB.getConnection;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Main {
     public static void main(String[] args) {
         Connection conn = getConnection();
         closeConnection();
+
+        Department department = new Department(1, "BOOKS");
+        showMessageDialog(null, department);
+
     }
 }
